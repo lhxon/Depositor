df0570efa599c0bad10d45ca6bc6b086d0ac2331
jack <guozhennianhua@163.com>
2016-08-01 20:32:56 +0800

    lf home update

--- Depositer/Program.cs
+++ Depositer/Program.cs
@@ -15,7 +15,7 @@ namespace Depositer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new Forms.MainForm());
         }
     }
 }
